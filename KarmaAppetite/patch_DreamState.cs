81b866ff0f8375d724c2c16ed184792c892d47f2
Dark-Gran <filip.randak@gmail.com>
2021-01-08 03:56:48 +0100

    removed SB dream

--- KarmaAppetite/patch_DreamState.cs
+++ KarmaAppetite/patch_DreamState.cs
@@ -10,7 +10,7 @@ public class patch_DreamState
     private static void DreamsState_EndOfCycleProgress(On.DreamsState.orig_EndOfCycleProgress orig, DreamsState self, SaveState saveState, string currentRegion, string denPosition)
     {
         bool everSleptInSB = true;
-        bool everSleptInSB_S = self.everSleptInSB_S01;
+        bool everSleptInSB_S = true;
         DreamsState.StaticEndOfCycleProgress(saveState, currentRegion, denPosition, ref self.integers[0], ref self.integers[1], ref self.integers[2], ref self.integers[5], ref self.upcomingDream, ref self.eventDream, ref everSleptInSB, ref everSleptInSB_S, ref self.guideHasShownHimselfToPlayer, ref self.integers[4], ref self.guideHasShownMoonThisRound, ref self.integers[3]);
         self.everSleptInSB = everSleptInSB;
         self.everSleptInSB_S01 = everSleptInSB_S;
